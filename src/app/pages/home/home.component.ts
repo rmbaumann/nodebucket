dc467ed1aa71954cf50ddd75b61a08cc8fa9ea73
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-12-01 17:36:01 -0600

    Update home.component.ts

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,26 @@
+/*
+=============================================
+; Title: nodebucket
+; Author: Reva Baumann
+; Date: 1 December 2019
+; Modified by: Reva Baumann
+; Description: nodebucket task management system
+;============================================
+*/
+
+// start program
+
+// Import the module
 import { Component, OnInit } from '@angular/core';
 
+// List detals of component
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
+
+// export
 export class HomeComponent implements OnInit {
 
   constructor() { }
@@ -13,3 +29,5 @@ export class HomeComponent implements OnInit {
   }
 
 }
+
+// end program
