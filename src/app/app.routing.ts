4b1b3efc90adcc2edbe24823a7993993e5e7f13d
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-12-01 17:36:04 -0600

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,23 @@
+/*
+=============================================
+; Title: nodebucket
+; Author: Reva Baumann
+; Date: 1 December 2019
+; Modified by: Reva Baumann
+; Description: nodebucket task management system
+;============================================
+*/
+
+// start program
+
+// Import the Modules, including Sign In and Auth
+
 import {Routes} from '@angular/router';
 import {BaseLayoutComponent} from './shared/base-layout/base-layout.component';
 import {HomeComponent} from './pages/home/home.component';
+import { AuthGuard } from './shared/guards/auth.guard';
+import { AuthLayoutComponent } from './shared/auth-layout/auth-layout.component';
+import { SigninComponent } from './pages/signin/signin.component';
 
 export const AppRoutes: Routes = [
   {
@@ -9,11 +26,25 @@ export const AppRoutes: Routes = [
     children: [
       {
         path: '',
-        component: HomeComponent
+        component: HomeComponent,
+        canActivate: [AuthGuard]
       }
-      /*
-        New components go here...
-       */
+    ]
+  },
+  /*
+  ** Details of AuthLayout & SignIn Compnoent
+  */
+  {
+    path: 'session',
+    component: AuthLayoutComponent,
+    children: [
+      {
+        path: 'signin',
+        component: SigninComponent
+      }
     ]
   }
+
 ];
+
+// End Program
