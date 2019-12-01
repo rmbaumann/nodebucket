c9c842db9fe38b2d6f9b6ac4812ebad848bd9f05
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-12-01 17:35:44 -0600

    Create auth.guard.ts

---
+++ src/app/shared/guards/auth.guard.ts
@@ -0,0 +1,45 @@
+/*
+=============================================
+; Title: nodebucket
+; Author: Reva Baumann
+; Date: 1 December 2019
+; Modified by: Reva Baumann
+; Description: nodebucket task management system
+;============================================
+*/
+
+// start program
+
+// Import the modules
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { Observable } from 'rxjs';
+
+//use injectables to implement cookie service
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+  constructor(private router: Router, private cookieService: CookieService) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      const sessionUser = this.cookieService.get('session_user');
+
+      /**
+       * True returns user
+       */
+      if (sessionUser) {
+        return true;
+      }
+      /**
+       * else navigates user to failed page
+       */
+      else {
+        this.router.navigate(['session/signin']);
+      }
+  }
+}
+// end program
