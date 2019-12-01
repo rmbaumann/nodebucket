52aa5120f0bdac6d945219d69a79eef6edadb035
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-12-01 17:36:07 -0600

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
+/*
+=============================================
+; Title: nodebucket
+; Author: Reva Baumann
+; Date: 1 December 2019
+; Modified by: Reva Baumann
+; Description: nodebucket task management system
+;============================================
+*/
+
+// start program
+
+// Import the module
 import { Component } from '@angular/core';
 
+// Router Outlet component
 @Component({
   selector: 'app-root',
   template: `<router-outlet></router-outlet>`,
   styles: [``]
 })
+
+// Export the class
 export class AppComponent {
   title = 'nodebucket';
 }
