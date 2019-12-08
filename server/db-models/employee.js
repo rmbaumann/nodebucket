57deccf60067986069abccfa1831121c994fc0ba
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-12-08 17:57:46 -0600

    Update employee.js

--- server/db-models/employee.js
+++ server/db-models/employee.js
@@ -13,14 +13,17 @@
 const mongoose = require('mongoose');
 const Task = require('./task')
 
-let employeeSchema = mongoose.Schema({
-  empId: {type: String, unique: true, dropDups: true},
-  firstname: {type: String},
-  lastname: {type: String},
-  todo: [task],
-  done: [task]
- });
+// Declare Emp database Schema
 
+let employeeSchema = mongoose.Schema({
+  empId:     {type: String, unique: true, dropDups: true},
+  firstname: {type: String},
+  lastname:  {type: String},
+  todo:      [Task],
+  done:      [Task]
+});
+
+ // Export the module
  module.exports = mongoose.model('Employee', employeeSchema);
 
  // End program
