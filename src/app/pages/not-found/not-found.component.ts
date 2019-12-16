b39a4f10049e97cc043d0c5374a04cd6c9c49aba
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-12-15 20:15:42 -0600

    Create not-found.component.ts

---
+++ src/app/pages/not-found/not-found.component.ts
@@ -0,0 +1,31 @@
+/*
+=============================================
+; Title: nodebucket
+; Author: Reva Baumann
+; Date: 1 December 2019
+; Modified by: Reva Baumann
+; Description: nodebucket task management system
+;============================================
+*/
+
+// start program
+
+// import the module
+import { Component, OnInit } from '@angular/core';
+
+// declare the component
+@Component({
+  selector: 'app-not-found',
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
+})
+export class NotFoundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+// end program
