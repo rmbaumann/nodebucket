2802c70b1db5ea96a26f1254948c763db3495601
Reva Baumann <rmbaumann@my365.bellevue.edu>
2019-12-15 20:15:56 -0600

    Create about.component.ts

---
+++ src/app/pages/about/about.component.ts
@@ -0,0 +1,32 @@
+/*
+=============================================
+; Title: nodebucket
+; Author: Reva Baumann
+; Date: 1 December 2019
+; Modified by: Reva Baumann
+; Description: nodebucket task management system
+;============================================
+*/
+
+
+// start program
+
+// Import the modules
+import { Component, OnInit } from '@angular/core';
+
+// Component
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+// end program
